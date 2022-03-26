579df7777e1909fb4512b902a21c10dd91c5e161
Callum Abbott <callum.abbott@outlook.com>
2022-03-26 15:49:31 +0000

    Added macro to aggregate by distinct column values :fire:

--- greenery/models/marts/product/intermediate/int_user_session_activity.sql
+++ greenery/models/marts/product/intermediate/int_user_session_activity.sql
@@ -12,17 +12,7 @@ select
     session_id,
     event_created_at,
     user_id,
-
-    /* 
-        This could be a macro but I think the code is clearer it like this since you
-        explicitly see the event types. Extra lines of SQL are cheap however human brain
-        power is expensive
-    */
-    sum(case when event_type='page_view' then 1 else 0 end) as page_view,
-    sum(case when event_type='add_to_cart' then 1 else 0 end) as add_to_cart,
-    sum(case when event_type='checkout' then 1 else 0 end) as checkout,
-    sum(case when event_type='package_shipped' then 1 else 0 end) as package_shipped
+    {{ agg_by_col('events', 'event_type') }}
 from
     events
-
 {{ dbt_utils.group_by(3) }}
