8ebed4a961d59da0984dff537557eb5b3dd4e6d6
Callum Abbott <callum.abbott@outlook.com>
2022-04-02 17:00:57 +0000

    product funnel created :rocket:

---
+++ greenery/models/marts/product/product_funnel.sql
@@ -0,0 +1,92 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with sessions as (
+    select * from {{ ref('fct_user_sessions')}}
+),
+
+/* 
+    The funnel for sessions on Greeney is defined as:
+    (1) Page View --> (2) Add to Cart --> (3) Checkout
+
+    NOTE: The at_least_{funnel_level} naming convention defines
+    a set of the levels including the specified {funnel_level} and
+    all those below it i.e. at_least_add_to_cart denotes the set
+    {add_to_cart, checkout}
+*/
+funnel as (
+    select 
+        'at_least_page_view' as funnel_level_label,
+        1 as funnel_level_id,
+
+        count(
+            distinct case when (
+                page_view > 0
+                or add_to_cart > 0
+                or checkout > 0
+                ) then session_id
+                else null
+            end
+        ) as num_sessions
+    from 
+        sessions
+
+    union all
+
+    select 
+        'at_least_add_to_cart' as funnel_level_label,
+        2 as funnel_level_id,
+
+        count(
+            distinct case when (
+                add_to_cart > 0
+                or checkout > 0
+                ) then session_id
+                else null
+            end
+        ) as num_sessions
+    from 
+        sessions
+    
+    union all
+    
+    select 
+        'at_least_checkout' as funnel_level_label,
+        3 as funnel_level_id,
+
+        count(
+            distinct case when (
+                checkout > 0
+                ) then session_id
+                else null
+            end
+        ) as num_sessions
+    from 
+        sessions
+),
+
+funnel_summary as (
+    select
+        funnel_level_label,
+        funnel_level_id,
+        num_sessions,
+        lag(num_sessions) OVER() as num_sessions_level_above
+    from
+        funnel
+),
+
+final as (
+    select
+        funnel_level_label,
+        funnel_level_id,
+        num_sessions,
+        num_sessions_level_above,
+        round(num_sessions::numeric / num_sessions_level_above::numeric, 2) as level_conversion
+    from
+        funnel_summary
+)
+
+select * from final
